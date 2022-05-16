88ed360b25790d74c39bcdfda9d3a99aa14cb601
kimpazes <kimpazs@gmail.com>
2022-05-16 20:40:10 +0200

    Hamburguer menu in mobile version

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello world! This is driving me insane!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
